eafa571d36ff9e2a30a3aeb6ff7027a8f9cdc8da
root <gaupande21@gmail.com>
2025-05-07 19:04:28 +0530

    updated code

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import logging 
 from glob_logging import CustomLogFW
 
+
 global_logger = CustomLogFW(service_name="main", instance_id="1")
 handler = global_logger.setup_logging()
 logging.getLogger().addHandler(handler)
-
-# logging.error("This is an info message from main")
-
 
 if __name__ == "__main__":
     logging.error("This is a error message from main")
